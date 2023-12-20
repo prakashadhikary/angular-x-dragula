804f2c6ea76f0ed6ec9ccd12c68181014184feac
Prakash Adhikari <prakash.adhikari@itonics.de>
2023-12-20 16:42:15 +0545

    Dragula with angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,70 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-starter';
+  dataWithoutSubGroup: any = [
+    {
+      id: 'active',
+      label: 'Active',
+      subGroup: [],
+    },
+    {
+      label: 'Inactive',
+      id: 'inactive',
+      subGroup: [],
+    },
+  ];
+
+  dataWithSubGroup: any = [
+    {
+      id: 'active',
+      label: 'Active',
+      subGroup: [
+        {
+          id: 'buybux_less',
+          label: 'Buybox Less Than',
+          subGroup: [
+            {
+              id: 'buybux_less',
+              label: 'Buybox Less Than',
+              subGroup: [
+                { id: 'buybux_less', label: 'Buybox Less Than' },
+                { id: 'buybux_greater', label: 'Buybux Greater Than' },
+              ],
+            },
+            { id: 'buybux_greater', label: 'Buybux Greater Than' },
+          ],
+        },
+        { id: 'buybux_greater', label: 'Buybux Greater Than' },
+      ],
+    },
+    {
+      label: 'Inactive',
+      id: 'inactive',
+      subGroup: [
+        { id: 'buybux_less', label: 'Buybox Less Than' },
+        { id: 'buybux_greater', label: 'Buybux Greater Than' },
+      ],
+    },
+  ];
+
+  normalizedGroup: any = {};
+
+  ngOnInit(): void {
+    this.dataWithSubGroup.forEach((group: any) => {
+      if (group.hasOwnProperty('subGroup') && group.subGroup.length) {
+        // If sub group exists
+        group.subGroup.forEach((sg: any) => {
+          const machineId = `${group.id}_${sg.id}`;
+          this.normalizedGroup[machineId] = [];
+        });
+      } else {
+        this.normalizedGroup[group.id] = [];
+      }
+    });
+
+    console.log(this.normalizedGroup);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DragDropComponent } from './drag-drop/drag-drop.component';
+import { DragulaModule } from 'ng2-dragula';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DragDropComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DragulaModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
